31514892b941327d186d6df87b58132a0b4ff8d1
Akshay pandit <51216244+panditakshay402@users.noreply.github.com>
2023-05-02 10:55:45 +0530

    backend completed

---
+++ insert.php
@@ -0,0 +1,28 @@
+<?php
+// Connect to MySQL database
+$conn = mysqli_connect("localhost", "root", "", "users");
+
+// Check for errors
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Get values from the HTML form
+$name = $_POST['name'];
+$email = $_POST['email'];
+$department = $_POST['department'];
+$time = $_POST['time'];
+
+
+// Insert data into MySQL table
+$sql = "INSERT INTO appointments (name, email, department,time) VALUES ('$name', '$email', '$department', 'time')";
+
+if (mysqli_query($conn, $sql)) {
+    echo "Appointment Booked successfully!";
+} else {
+    echo "Failed Becaused of  " . $sql . "<br>" . mysqli_error($conn);
+}
+
+// Close MySQL connection
+mysqli_close($conn);
+?>
